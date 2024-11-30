df390bb4fd06fb2c122d9151e142e3038fba7cbe
Jalal Abdul Aziz <jalalabdulaziz@gmail.com>
2024-11-30 11:58:48 +0800

    Updated user.js

--- user.js
+++ user.js
@@ -1,10 +1,10 @@
-/** UI **/
+/** Theme & UI **/
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.theme.native-theme", true);
 user_pref("widget.non-native-theme.scrollbar.style", 2);
 user_pref("browser.aboutConfig.showWarning", false);
 
-/** TELEMETRY **/
+/** Telemetry **/
 user_pref("datareporting.policy.dataSubmissionEnabled", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
 user_pref("toolkit.telemetry.unified", false);
@@ -22,11 +22,20 @@ user_pref("toolkit.coverage.endpoint.base", "");
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
 
-/** MISC **/
-user_pref("extensions.pocket.enabled", false);
-user_pref("media.hardware-video-decoding.enabled", true);
+/** New tab page **/
 user_pref("browser.newtabpage.enabled", false);
 user_pref("browser.startup.homepage", "about:blank");
 user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
 user_pref("browser.newtabpage.activity-stream.showWeather", false);
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+
+/** Search & URL bar **/
+user_pref("browser.urlbar.trimHttps", true);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.quicksuggest.enabled", false);
+
+/** Misc **/
+user_pref("extensions.pocket.enabled", false);
+user_pref("sidebar.revamp", false);
+user_pref("media.hardware-video-decoding.enabled", true);
+user_pref("media.av1.enabled", false);
